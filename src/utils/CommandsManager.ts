1ea7df1a926a6f34a2d1f5d6dffc4780256a31ef
Wailrone <36567701+Wailrone@users.noreply.github.com>
2022-08-28 18:47:56 +0200

    :bug: Better checking of commands options

--- src/utils/CommandsManager.ts
+++ src/utils/CommandsManager.ts
@@ -4,7 +4,7 @@
 
 import Client from "../../main";
 import {resolve} from "path";
-import {ApplicationCommandManager, Collection, ApplicationCommandType} from "discord.js";
+import {ApplicationCommandManager, ApplicationCommandType, Collection} from "discord.js";
 import {access, readdir, stat} from "fs/promises";
 import Command from "./Command";
 
@@ -72,23 +72,37 @@ class CommandsManager {
             }
         }
 
+        function areSameOptions(a1: any[], a2: any[]): boolean {
+            function washUndefinedAndFalse(objs: any[]) {
+                return objs?.map(obj => Object.fromEntries(Object.entries(obj).filter(e => e[1])))
+            }
 
-        if (this._globalCommands.cache.some(cmd =>
-            (this._commands.get(cmd.name)?.description || "") !== cmd?.description ||
-            (this._commands.get(cmd.name)?.options || []) !== cmd?.options
+            function objectsEqual(o1: any, o2: any): boolean {
+                return typeof o1 === 'object' && Object.keys(o1).length > 0
+                    ? Object.keys(o1).length === Object.keys(o2).length
+                    && Object.keys(o1).every(p => objectsEqual(o1[p], o2[p]))
+                    : o1 === o2;
+            }
+
+            a1 = washUndefinedAndFalse(a1);
+            a2 = washUndefinedAndFalse(a2);
+            return a1?.length === a2?.length && a1?.every((obj, i) => objectsEqual(obj, a2[i]));
+        }
+
+        if (this._globalCommands.cache.some(cmd => (this._commands.get(cmd.name)?.description || "") !== cmd?.description || !areSameOptions(this._commands.get(cmd.name)?.options || [], cmd.options || [])
         ) || this._globalCommands.cache.size !== this._commands.size) {
             await this._globalCommands.set(this._commands.map((cmd) => {
                 if (cmd.type !== ApplicationCommandType.ChatInput) return {
                     name: cmd.name,
                     type: cmd.type,
-                    defaultMemberPermissions : cmd.userPerms,
+                    defaultMemberPermissions: cmd.userPerms,
                 }
                 else return {
                     name: cmd.name,
                     type: cmd.type,
                     description: cmd.description,
                     options: cmd.options,
-                    defaultMemberPermissions : cmd.userPerms,
+                    defaultMemberPermissions: cmd.userPerms,
                 }
             }));
             console.info("[Commands] Refresh the globals commands");
